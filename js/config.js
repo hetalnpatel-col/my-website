854fb743b8e935064143663570bfce49705cd2cf
hetalnpatel-col <78589290+hetalnpatel-col@users.noreply.github.com>
2026-07-12 14:05:57 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 const API_URL =
-"https://script.google.com/a/macros/adit.ac.in/s/AKfycbxglG1DmRhWFGFu3i-6cqRy87kIaKSadhF4Waf6FEykJJ94049vzpmTInCXSmNEW8i_pA/exec";
+"https://script.google.com/macros/s/AKfycbxglG1DmRhWFGFu3i-6cqRy87kIaKSadhF4Waf6FEykJJ94049vzpmTInCXSmNEW8i_pA/exec";
